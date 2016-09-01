69f29a090f824dbdbe6f30617c0a93da436e490a
Dydkina <dydkinaa@gmail.com>
2016-09-01 22:02:13 +0400

    Все
    завершила

---
+++ js/popup.js
@@ -0,0 +1,20 @@
+	  	var btn = document.querySelector(".in-sedona");
+	  	var popup = document.querySelector(".modal-content");
+ 			var date = popup.querySelector("[name=date-in]");
+ 			
+      btn.addEventListener("click", function(event) {
+  event.preventDefault();
+  if (!popup.classList.contains("modal-content-show")){
+    popup.classList.add("modal-content-show");
+  } else {
+    popup.classList.remove("modal-content-show");
+  }
+})
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode == 27) {
+    if (popup.classList.contains("modal-content-show")) {
+      popup.classList.remove("modal-content-show");
+    }
+  }
+});  
